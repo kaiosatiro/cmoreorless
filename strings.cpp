6711f44ed6b798533771b9575ca554bd8123889f
kaiosatiro <kaiosatiro@gmail.com>
2024-10-14 19:23:10 -0300

    strings

---
+++ strings.cpp
@@ -0,0 +1,22 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    string a;
+    string b;
+
+    cin >> a >> b;
+    cout << a.size()<< " " << b.size() << endl;
+    cout << a + b << endl;
+
+    swap(a[0], b[0]);
+    cout << a << " " << b;
+    
+
+    return 0;
+}
